6a6d9905c7645bead7492e1af88689313e818dc1
ZaraKnYu <razenxizu@gmail.com>
2026-05-16 21:45:01 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,8 +11,8 @@ rm -rf hardware/motorola
 rm -rf packages/apps/DolbyUI
 rm -rf hardware/dolby
 
-git clone https://github.com/motorola-sm6435/android_device_motorola_mumba -b test device/motorola/mumba
-git clone https://github.com/motorola-sm6435/android_vendor_motorola_mumba -b test vendor/motorola/mumba
+git clone https://github.com/motorola-sm6435/android_device_motorola_mumba device/motorola/mumba
+git clone https://github.com/motorola-sm6435/android_vendor_motorola_mumba vendor/motorola/mumba
 git clone https://github.com/motorola-sm6435/android_device_motorola_mumba-kernel device/motorola/mumba-kernel
 git clone https://github.com/LineageOS/android_hardware_motorola -b lineage-23.2 hardware/motorola
 git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.5 hardware/dolby
