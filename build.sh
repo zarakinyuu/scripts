3347e3b60b2543cea7945b333f93804808565058
ZaraKinYu <razenxizu@gmail.com>
2026-07-01 15:13:36 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,3 +15,16 @@ git clone https://github.com/LineageOS/android_hardware_motorola -b lineage-23.2
 . build/envsetup.sh
 lunch lineage_mumba-bp4a-userdebug
 m evolution
+
+
+
+
+git clone https://github.com/mumba4ever/android_device_motorola_mumba -b oss-kernel device/motorola/mumba
+git clone https://github.com/mumba4ever/android_vendor_motorola_mumba vendor/motorola/mumba
+git clone https://github.com/mumba4ever/android_kernel_motorola_sm8450 kernel/motorola/sm8450
+git clone https://github.com/mumba4ever/android_kernel_motorola_sm8450-modules kernel/motorola/sm8450-modules
+git clone https://github.com/mumba4ever/android_kernel_motorola_sm8450-devicetrees kernel/motorola/sm8450-devicetrees
+git clone https://github.com/LineageOS/android_hardware_motorola -b lineage-23.2 hardware/motorola
+
+
+
