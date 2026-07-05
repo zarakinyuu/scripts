92bfd98afaef3b6daf02752270ac0b346cc88400
ZaraKinYu <razenxizu@gmail.com>
2026-07-05 22:40:46 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,3 +16,21 @@ git clone https://github.com/LineageOS/android_hardware_motorola -b lineage-23.2
 . build/envsetup.sh
 lunch lineage_mumba-bp4a-userdebug
 mka bacon -j$(nproc --all)
+
+
+
+
+
+rm -rf kernel/motorola/sm6435
+rm -rf kernel/motorola/sm6435-devicetrees
+rm -rf kernel/motorola/sm6435-modules
+rm -rf hardware/motorola
+rm -rf device/motorola/mumba
+rm -rf vendor/motorola/mumba
+
+git clone https://github.com/mumba4ever/android_device_motorola_mumba -b oss device/motorola/mumba
+git clone https://github.com/mumba4ever/android_vendor_motorola_mumba vendor/motorola/mumba
+git clone https://github.com/LineageOS/android_hardware_motorola -b lineage-23.2 hardware/motorola
+git clone https://github.com/ZaraKinYu-Playground/android_kernel_motorola_sm6435 kernel/motorola/sm6435
+git clone https://github.com/ZaraKinYu-Playground/android_kernel_motorola_sm6435-modules kernel/motorola/sm6435-modules
+git clone https://github.com/ZaraKinYu-Playground/android_kernel_motorola_sm6435-devicetrees kernel/motorola/sm6435-devicetrees
