fd88c30d428d1292df4b3292041e7eb9b6b99df8
ZaraKnYu <razenxizu@gmail.com>
2026-07-23 22:36:33 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,6 +12,8 @@ git clone https://github.com/mumba4ever/android_device_motorola_mumba device/mot
 git clone https://github.com/mumba4ever/android_vendor_motorola_mumba vendor/motorola/mumba
 git clone https://github.com/mumba4ever/android_device_motorola_mumba-kernel device/motorola/mumba-kernel
 git clone https://github.com/LineageOS/android_hardware_motorola -b lineage-23.2 hardware/motorola
+
+rm -rf vendor/motorola/mumba-motcamera5 device/motorola/mumba-motcamera5
 git clone https://github.com/mumba4ever/android_vendor_motorola_mumba-motcamera5 vendor/motorola/mumba-motcamera5
 git clone https://github.com/mumba4ever/android_device_motorola_mumba-motcamera5 device/motorola/mumba-motcamera5
 
