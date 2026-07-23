83f7bb2e3a2f95b462de3bdb2505f137f5ccca20
ZaraKnYu <razenxizu@gmail.com>
2026-07-24 02:47:00 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,20 +3,14 @@
 repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
 /opt/crave/resync.sh
 
-rm -rf hardware/motorola
-rm -rf device/motorola/mumba-kernel
-rm -rf device/motorola/mumba
-rm -rf vendor/motorola/mumba
-
+rm -rf vendor/motorola/mumba-motcamera5 device/motorola/mumba-motcamera5 vendor/motorola/mumba device/motorola/mumba device/motorola/mumba-kernel hardware/motorola
 git clone https://github.com/mumba4ever/android_device_motorola_mumba device/motorola/mumba
 git clone https://github.com/mumba4ever/android_vendor_motorola_mumba vendor/motorola/mumba
 git clone https://github.com/mumba4ever/android_device_motorola_mumba-kernel device/motorola/mumba-kernel
-git clone https://github.com/LineageOS/android_hardware_motorola -b lineage-23.2 hardware/motorola
-
-rm -rf vendor/motorola/mumba-motcamera5 device/motorola/mumba-motcamera5
+git clone https://github.com/mumba4ever/android_hardware_motorola hardware/motorola
 git clone https://github.com/mumba4ever/android_vendor_motorola_mumba-motcamera5 vendor/motorola/mumba-motcamera5
 git clone https://github.com/mumba4ever/android_device_motorola_mumba-motcamera5 device/motorola/mumba-motcamera5
 
 . build/envsetup.sh
-lunch lineage_mumba-bp4a-eng
+lunch lineage_mumba-bp4a-userdebug
 mka bacon
